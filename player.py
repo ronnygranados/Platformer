0b775bea55c99c33a5fa1df8a02fd28cb08dc20f
ronnygranados <ronnygranados7@gmail.com>
2022-01-10 00:13:16 -0600

    graphics folder

---
+++ player.py
@@ -0,0 +1,45 @@
+import pygame
+from variables import *
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.Surface((32, 64))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect(topleft=pos)
+
+        # player movement
+        self.direction = pygame.math.Vector2(0, 0)
+        self.speed = 8
+        self.gravity = 0.8
+        self.jump_speed = -16
+
+    # def import_character_assets(self):
+    #     character_path = '../graphics/character/'
+    #     self.animations = {'idle': [], 'run': [], 'jump': [], 'fall':[]}
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+        if keys[pygame.K_SPACE]:
+            self.jump()
+
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+
+    def jump(self):
+        self.direction.y = self.jump_speed
+
+    def update(self):
+        self.get_input()
+        # self.rect.x += self.direction.x * self.speed
+        # self.apply_gravity()
