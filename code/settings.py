3036ec4601dc527a6b9b9dd60a643ef50e54abd9
ronnygranados <ronnygranados7@gmail.com>
2022-01-10 00:11:20 -0600

    graphics folder

---
+++ code/settings.py
@@ -0,0 +1,16 @@
+level_map = [
+'                            ',
+'                            ',
+'                            ',
+' XX    XXX            XX    ',
+' XX P                       ',
+' XXXX         XX         XX ',
+' XXXX       XX              ',
+' XX    X  XXXX    XX  XX    ',
+'       X  XXXX    XX  XXX   ',
+'    XXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+TILE_SIZE = 64
+WIDTH = 1200
+HEIGHT = len(level_map) * TILE_SIZE
